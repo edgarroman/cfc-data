a5a715a11175f4588e87e05d2e16e0b38be899cc
Edgar Roman <edgar@edgarroman.com>
2018-09-16 15:30:20 -0400

    Upgraded packages

--- cfc/apps/core/models.py
+++ cfc/apps/core/models.py
@@ -6,61 +6,74 @@ class TimeStampedModel(models.Model):
     An abstract base class model that provides self-
     updating ``created`` and ``modified`` fields.
     """
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
+
 class Person(TimeStampedModel):
     name = models.CharField(max_length=255)
     email = models.EmailField
+
     def __str__(self):
         return self.name
+
 
 class League(TimeStampedModel):
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
+
 class Team(TimeStampedModel):
-    club = models.CharField(max_length=255,blank=True)
+    club = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255)
-    squad = models.CharField(max_length=255,blank=True)
+    squad = models.CharField(max_length=255, blank=True)
     age_division = models.CharField(max_length=255, blank=True)
-    league = models.ForeignKey(League)
-    league_key = models.CharField(max_length=255,blank=True)
-    url = models.URLField(blank=True,null=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    league_key = models.CharField(max_length=255, blank=True)
+    url = models.URLField(blank=True, null=True)
+
     def __str__(self):
         return self.name
+
 
 class Field(TimeStampedModel):
     name = models.CharField(max_length=255)
     cfc_home_field = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
+
 
 class LeagueField(TimeStampedModel):
     name = models.CharField(max_length=255)
-    league_key = models.CharField(max_length=255,blank=True)
-    league = models.ForeignKey(League)
-    field = models.ForeignKey(Field)
+    league_key = models.CharField(max_length=255, blank=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
+
 class Game(TimeStampedModel):
-    home_team = models.ForeignKey(Team,related_name='home')
-    away_team = models.ForeignKey(Team,related_name='away')
-    field = models.ForeignKey(Field)
+    home_team = models.ForeignKey(Team, related_name="home", on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, related_name="away", on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
     schedule = models.DateTimeField()
-    league = models.ForeignKey(League)
-    league_key = models.CharField(max_length=255,blank=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    league_key = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return (self.schedule.strftime("%m/%d %I:%M") + 
-                ' - ' + 
-                str(self.home_team) + 
-                ' vs. ' + 
-                str(self.away_team))
-
+        return (
+            self.schedule.strftime("%m/%d %I:%M")
+            + " - "
+            + str(self.home_team)
+            + " vs. "
+            + str(self.away_team)
+        )
 
--- cfc/settings.py
+++ cfc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ulzdg%y02jjjug(*k5ctn4pud8iz$^78$bkjsxifq%*=b%pkz='
+SECRET_KEY = "ulzdg%y02jjjug(*k5ctn4pud8iz$^78$bkjsxifq%*=b%pkz="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,55 +31,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize', # for easy template tag helpers
-    'cfc.apps.core',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",  # for easy template tag helpers
+    "cfc.apps.core",
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'cfc.urls'
+ROOT_URLCONF = "cfc.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'cfc.wsgi.application'
+WSGI_APPLICATION = "cfc.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'work_local', 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "work_local", "db.sqlite3"),
     }
 }
 
@@ -88,27 +87,19 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
@@ -118,13 +109,13 @@ USE_TZ = True
 
 # ======================================================
 LEAGUE_CFC_PAGES = [
-    { 'league' : 'ODSL', 'url' : 'http://elements.demosphere.com/91918/teams/club/17029528.html'},
-    { 'league' : 'NCSL', 'url' : 'http://elements.demosphere.com/80738/teams/club/15599440.html'},
+    {"league": "ODSL", "url": "http://elements.demosphere.com/91918/teams/club/17029528.html"},
+    {"league": "NCSL", "url": "http://elements.demosphere.com/80738/teams/club/15599440.html"},
 ]
 
 # EDP Teams are a PITA so we have to list them individually
 LEAGUE_EDP_TEAMS = []
-'''
+"""
     {
         'team' : 'CAPITAL FUTBOL STRIKERS RED',
 #        'team' : 'CAPITALFC STRIKERS RED',
@@ -147,37 +138,32 @@ LEAGUE_EDP_TEAMS = []
         'url' : 'http://events.gotsport.com/events/schedule.aspx?eventid=57134&FieldID=0&applicationID=3826507&action=Go'
     },
 ]
-'''
+"""
 
 FIELD_ALIAS = {
-    'Watkins ES' : 'Watkins',
-    'Watkins Elementary School  DC #1' : 'Watkins',
-    'Watkins Elementary School   DC #1' : 'Watkins',
-    'Watkins Rec Center' : 'Watkins',
-
-    'Ridge Road Rec Center': 'Ridge Road Rec Center',
-    'Ridge Road Rec Center  DC Full sided field': 'Ridge Road Rec Center',
-    'Ridge Road Rec Center  DC full-sided field': 'Ridge Road Rec Center', # Warning control char included
-    'Ridge Rd Recreation DC #1' : 'Ridge Road Rec Center',
-
-    'Randall Recreation Center Randall Full sided field': 'Randall Rec Center',
-    'Randall Recreation Center #1 Full': 'Randall Rec Center',
-    'Randall Recreation Center Soccer Field': 'Randall Rec Center',
-    'Randall Rec Center': 'Randall Rec Center',
-    'Randall Junior High School #1': 'Randall Rec Center',
-
-    'Trinidad Recreation Center Full Sided': 'Trinidad Rec Center',
-    'Trinidad Rec Center': 'Trinidad Rec Center',
-    'Trinidad Rec. Center': 'Trinidad Rec Center',
-    'Trinidad Rec Center #1' : 'Trinidad Rec Center',
-
-    'Jefferson Middle School' : 'Jefferson Middle School',
-
-    'Rosedale Rec Center' : 'Rosedale Rec Center',
-    'Rosedale Rec Center, DC Small sided' : 'Rosedale Rec Center',
-    'Rosedale Rec Center  DC Small sided field 1' : 'Rosedale Rec Center',
-    'Rosedale Rec Center, DC full sided field' : 'Rosedale Rec Center',
-    'Rosedale Recreation Center Field' : 'Rosedale Rec Center',
+    "Watkins ES": "Watkins",
+    "Watkins Elementary School  DC #1": "Watkins",
+    "Watkins Elementary School   DC #1": "Watkins",
+    "Watkins Rec Center": "Watkins",
+    "Ridge Road Rec Center": "Ridge Road Rec Center",
+    "Ridge Road Rec Center  DC Full sided field": "Ridge Road Rec Center",
+    "Ridge Road Rec Center  DC full-sided field": "Ridge Road Rec Center",  # Warning control char included
+    "Ridge Rd Recreation DC #1": "Ridge Road Rec Center",
+    "Randall Recreation Center Randall Full sided field": "Randall Rec Center",
+    "Randall Recreation Center #1 Full": "Randall Rec Center",
+    "Randall Recreation Center Soccer Field": "Randall Rec Center",
+    "Randall Rec Center": "Randall Rec Center",
+    "Randall Junior High School #1": "Randall Rec Center",
+    "Trinidad Recreation Center Full Sided": "Trinidad Rec Center",
+    "Trinidad Rec Center": "Trinidad Rec Center",
+    "Trinidad Rec. Center": "Trinidad Rec Center",
+    "Trinidad Rec Center #1": "Trinidad Rec Center",
+    "Jefferson Middle School": "Jefferson Middle School",
+    "Rosedale Rec Center": "Rosedale Rec Center",
+    "Rosedale Rec Center, DC Small sided": "Rosedale Rec Center",
+    "Rosedale Rec Center  DC Small sided field 1": "Rosedale Rec Center",
+    "Rosedale Rec Center, DC full sided field": "Rosedale Rec Center",
+    "Rosedale Recreation Center Field": "Rosedale Rec Center",
 }
 
 # =====================================================
@@ -186,8 +172,8 @@ FIELD_ALIAS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'work_local','static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "work_local", "static")
 
 # Logging
 # A sample logging configuration. The only tangible logging
@@ -201,59 +187,38 @@ U_LOGFILE_SIZE = 1 * 1024 * 1024
 U_LOGFILE_COUNT = 2
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "console": {"level": "INFO", "class": "logging.StreamHandler", "formatter": "simple"},
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "application.log",
+            "maxBytes": U_LOGFILE_SIZE,
+            "backupCount": U_LOGFILE_COUNT,
+            "formatter": "verbose",
+        },
     },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console':{
-            'level':'INFO',
-            'class':'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'application.log',
-            'maxBytes' : U_LOGFILE_SIZE,
-            'backupCount' : U_LOGFILE_COUNT,
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django': {
-            'handlers':['file','console'],
-            'level':'INFO',
-            'propagate': True,
-        },
+    "loggers": {
+        "django.request": {"handlers": ["mail_admins"], "level": "ERROR", "propagate": True},
+        "django": {"handlers": ["file", "console"], "level": "INFO", "propagate": True},
         ## Todo: this must be your app
-        'cfc.apps': {
-            'handlers': ['file','console'],
-            'level': 'INFO',
-        },
-    }
+        "cfc.apps": {"handlers": ["file", "console"], "level": "INFO"},
+    },
 }
 
 
